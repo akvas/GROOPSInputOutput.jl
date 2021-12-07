616c095d8d4b66cc6cb0c4e4cba59b0db27a7afb
Andreas Kvas <kvas@tugraz.at>
2021-12-07 08:55:53 +0100

    switched to pycall

--- src/GROOPSInputOutput.jl
+++ src/GROOPSInputOutput.jl
@@ -1,9 +1,26 @@
 module GROOPSInputOutput
-  using CxxWrap
-  @wrapmodule(joinpath(dirname(dirname(@__FILE__)), "deps", "lib", "libgroopsio"))
+
+  using PyCall
+
+  const gio = PyNULL()
 
   function __init__()
-    @initcxx
+    copy!(gio, pyimport("groopsio"))
   end
 
+  function loadmatrix(filename)
+    gio.loadmatrix(filename)
+  end
+
+  function savematrix(filename, matrix, matrix_type="general", lower=false)
+    gio.savematrix(filename, matrix, matrix_type=matrix_type, lower=lower)
+  end
+
+  function loadgrid(filename)
+    gio.loadgrid(filename)
+  end
+
+  function loadpolygon(filename)
+    gio.loadpolygon(filename)
+  end
 end
